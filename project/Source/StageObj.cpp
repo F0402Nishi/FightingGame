2d2de80b93e6a06f74b6294190e4ef5117ec9dbd
Nishi <SDT2395GP0016@edu.sca.ac.jp>
2025-03-29 17:20:40 +0900

    次回、StageObj用のCollderの作成から

---
+++ project/Source/StageObj.cpp
@@ -0,0 +1,46 @@
+#include "StageObj.h"
+#include <assert.h>
+
+StageObj::StageObj(int kind, VECTOR pos, VECTOR rot, VECTOR sca)
+{
+	char buf[256];
+	sprintf_s<256>(buf, "data/Map/Stage/Stage_Obj%03d.mv1", kind);
+	hModel = MV1LoadModel(buf);
+	sprintf_s<256>(buf, "data/Map/Stage/Stage_Obj%03d_c.mv1", kind);
+	hitModel = MV1LoadModel(buf);
+	assert(hitModel > 0);
+
+
+	transform.position = pos;
+	transform.rotation = rot;
+	transform.scale = sca;
+}
+
+StageObj::~StageObj()
+{
+}
+
+bool StageObj::SearchObject(VECTOR pos1, VECTOR pos2, VECTOR* hit)
+{
+	MV1SetMatrix(hModel, transform.matrix);
+	MV1RefreshCollInfo(hitModel);
+	MV1_COLL_RESULT_POLY ret = MV1CollCheck_Line(hitModel, -1, pos1, pos2);
+	if (ret.HitFlag) {
+		if (hit != nullptr) {
+			*hit = ret.HitPosition;
+		}
+		return true;
+	}
+	return false;
+}
+
+VECTOR StageObj::CollderObject(VECTOR pos, SphereCollder col)
+{
+	MV1_COLL_RESULT_POLY_DIM dim = MV1CollCheck_Sphere(hitModel, -1, col.center + pos, col.radius);
+	if (dim.HitNum == 0) return pos;
+	for (int i = 0; i < dim.HitNum; i++) {
+		VECTOR hitPos = dim.Dim[i].HitPosition;
+		VECTOR hitNorm = dim.Dim[i].Normal;
+	}
+	return VECTOR();
+}
--- project/Source/StageObj.h
+++ project/Source/StageObj.h
@@ -4,5 +4,12 @@
 class StageObj : public Object3D
 {
 public:
+	StageObj(int kind, VECTOR pos, VECTOR rot, VECTOR sca);
+	~StageObj();
+	bool SearchObject(VECTOR pos1, VECTOR pos2, VECTOR* hit = nullptr);
+	VECTOR CollderObject(VECTOR pos, SphereCollder col);
 
+
+private:
+	int hitModel;
 };
